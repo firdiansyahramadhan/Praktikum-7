29ccd8cb0ace24b1ff732fc1139ad9df34c9748d
firdiansyahramadhan <81859768+firdiansyahramadhan@users.noreply.github.com>
2021-05-15 07:33:38 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <title>Predefine Variable</title>
+</head>
+<body>
+     <h1>Predefine Variable</h1>
+<?php
+echo 'Selamat Datang ' . $_GET['nama'];
+?>
